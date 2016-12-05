11af425dcbe056db7e0be4d86f08af7ebe38a236
ChienHo <467360687@qq.com>
2016-12-05 20:49:32 +0800

    init

---
+++ PageManage/PageTable.swift
@@ -0,0 +1,16 @@
+//
+//  PageTable.swift
+//  PageManage
+//
+//  Created by 何成健 on 16/12/5.
+//  Copyright © 2016年 何成健. All rights reserved.
+//
+
+struct PageTable{
+    var no: Int
+    var block_no: Int
+    var P: Bool
+    var A: Bool
+    var M: Bool
+    var memory_address: Int
+}
--- PageManage/ViewController.swift
+++ PageManage/ViewController.swift
@@ -10,18 +10,16 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    let PAGE_SIZE: Int = 10//每个页面可存放10条指令
+    let MEMORY_SIZE: Int = 4//分配给作业的内存块数为4
+    let PROGRAM_SIZE: Int = 320//共320条指令
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    @IBAction func OnCommandBtnClick(_ sender: NSButton) {
+        
     }
-
 
 }
-
