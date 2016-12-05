cfed930ee71fdf37ec000ad255bb2e71e529d5ba
ChienHo <467360687@qq.com>
2016-12-05 15:21:59 +0800

    Initial Commit

---
+++ PageManage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PageManage
+//
+//  Created by 何成健 on 16/12/5.
+//  Copyright © 2016年 何成健. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PageManage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PageManage
+//
+//  Created by 何成健 on 16/12/5.
+//  Copyright © 2016年 何成健. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
